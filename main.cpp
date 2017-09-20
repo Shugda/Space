6f09f4d4563cc15bf33f4f3d50edfd11b0783365
Shugda <patyah@mail.ru>
2017-09-20 11:44:12 +0300

    Пентаграмма

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "glcanvas.hpp"
 using namespace cnv;
 
-float windowwidth = 400;
-float windowhigh = 400;
+float alpha = 0.0f;
+float a;
 
 void changeSize(int w, int h)
 {
@@ -14,83 +14,86 @@ void changeSize(int w, int h)
 	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
 	glMatrixMode(GL_MODELVIEW);
 }
-
 void Draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0f,1.0f,1.0f);
-	glBegin(GL_QUADS);
+
+	glLoadIdentity();
+	
+	gluLookAt(0.0f,0.0f,10.0f,
+			0.0f,0.0f,0.0f,
+			0.0f,1.0f,0.0f);
+	
+	glRotatef(alpha,1.0f,1.0f,1.0f);
+	
+	glColor3f(0.0f,1.0f,0.0f);
+	glBegin(GL_POLYGON);
 	{
-		glVertex3f(25.0f,25.0,-25.0);
-		glVertex3f(-25.0f,25.0,-25.0);
-		glVertex3f(-25.0f,-25.0,-25.0);
-		glVertex3f(25.0f,-25.0,-25.0);
+		glVertex3f(0.0f,1.0f,1.0f);
+		glVertex3f(-1.0f,0.2f,1.0f);
+		glVertex3f(-0.7f,-1.0f,1.0f);
+		glVertex3f(0.7f,-1.0f,1.0f);
+		glVertex3f(1.0f,0.2f,1.0f);
+	}	
+	glEnd();
 
-		glColor3f(0,1,0);
-		glVertex3f(25.0f,25.0,25.0);
-		glVertex3f(-25.0f,25.0,25.0);
-		glVertex3f(-25.0f,-25.0,25.0);
-		glVertex3f(25.0f,-25.0,25.0);
+	glBegin(GL_TRIANGLES);
+	{
+		glVertex3f(1.0f,0.2f,1.0f);			
+		glVertex3f(0.0f,1.0f,1.0f);			
+		glVertex3f(1.5f,1.7f,1.0f);			
+		
+		glVertex3f(0.0f,1.0f,1.0f);			
+		glVertex3f(-1.0f,0.2f,1.0f);			
+		glVertex3f(-1.5f,1.7f,1.0f);			
+		
+		glVertex3f(-1.0f,0.2f,1.0f);			
+		glVertex3f(-0.7f,-1.0f,1.0f);			
+		glVertex3f(-1.8f,-1.0f,1.0f);			
+		
+		glVertex3f(0.7f,-1.0f,1.0f);			
+		glVertex3f(1.0f,0.2f,1.0f);			
+		glVertex3f(1.8f,-1.0f,1.0f);			
+		
+		glVertex3f(-0.7f,-1.0f,1.0f);			
+		glVertex3f(0.7f,-1.0f,1.0f);			
+		glVertex3f(0.0f,-2.5f,1.0f);			
+	}
+	glEnd();
+	
+	glColor3f(1.0f,0.0f,0.0f);
 
-		glColor3f(0,0,1);
-		glVertex3f(-25.0f,25.0,25.0);
-		glVertex3f(-25.0f,25.0,-25.0);
-		glVertex3f(-25.0f,-25.0,-25.0);
-		glVertex3f(-25.0f,-25.0,25.0);
-
-		glColor3f(1,1,0);
-		glVertex3f(-25.0f,25.0,-25.0);
-		glVertex3f(-25.0f,25.0,25.0);
-		glVertex3f(25.0f,25.0,25.0);
-		glVertex3f(25.0f,25.0,-25.0);
-
-		glColor3f(1,0,1);
-		glVertex3f(25.0f,25.0,-25.0);
-		glVertex3f(25.0f,25.0,25.0);
-		glVertex3f(25.0f,-25.0,25.0);
-		glVertex3f(25.0f,-25.0,-25.0);
-
-		glColor3f(0,1,1);
-		glVertex3f(25.0f,-25.0,-25.0);
-		glVertex3f(-25.0f,-25.0,-25.0);
-		glVertex3f(-25.0f,-25.0,25.0);
-		glVertex3f(25.0f,-25.0,25.0);
-
+	glBegin(GL_LINES);
+	{
+		glVertex3f(1.5f,1.7f,1.0f);			
+		glVertex3f(-1.5f,1.7f,1.0f);			
+		glVertex3f(-1.5f,1.7f,1.0f);			
+		glVertex3f(-1.8f,-1.0f,1.0f);			
+		glVertex3f(-1.8f,-1.0f,1.0f);			
+		glVertex3f(0.0f,-2.5f,1.0f);			
+		glVertex3f(1.8f,-1.0f,1.0f);			
+		glVertex3f(1.5f,1.7f,1.0f);			
+		glVertex3f(0.0f,-2.5f,1.0f);			
+		glVertex3f(1.8f,-1.0f,1.0f);			
 	}
 	glEnd();
 
+	alpha+=1.0f;
+
 	glutSwapBuffers();
-}	
-
-void Timer(int)
-{
-	glRotatef(1,1.0,1.0,1.0);
-	glutPostRedisplay();
-	glutTimerFunc(10, Timer, 0);
-}
-
-void Initialize()
-{
-	glClearColor(0.0f,0.0f,0.0f,0.0f);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-windowwidth/2, windowwidth/2, -windowhigh/2,windowhigh/2,-200.0,200.0);
-	glMatrixMode(GL_MODELVIEW);
-	glTranslatef(0.0,0.0,0.0);
-	glScalef(1.0,1.0,1.0);
-	glRotatef(0,0.0,0.0,0.0);
 }
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);	
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(windowwidth,windowhigh);
-	glutInitWindowPosition(200,0);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(400,400);
+	glutInitWindowPosition(200,100);
 	glutCreateWindow("Lesson 1");
 
 	glutDisplayFunc(Draw);
-	Initialize();
-	Timer(0);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(Draw);
+	
 	glutMainLoop();
 }
